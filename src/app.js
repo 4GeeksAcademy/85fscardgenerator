fb09c5d0c748791de6e686d3682e92390652e4b0
kevinpg93 <keeviin.93@gmail.com>
2024-11-15 12:10:47 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const valores = ["2", "3", "4", "5", "6", "7", "8", "10", "J", "Q", "K", "A"];
+  const palos = ["♦", " ♥", "♠", "♣"];
+
+  const randomValores = Math.floor(Math.random() * valores.length);
+  const randomPalos = Math.floor(Math.random() * palos.length);
+
+  const palo1 = document.getElementById("palo");
+  palo1.innerText = palos[randomPalos];
+  const numero = document.getElementById("num");
+  numero.innerText = valores[randomValores];
+  const palo2 = document.getElementById("palo2");
+  palo2.innerText = palos[randomPalos];
 };
