b0ffac60be922d5e15001248322757352efce800
kevinpg93 <keeviin.93@gmail.com>
2024-11-15 13:47:12 +0000

    correciones

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   const valores = ["2", "3", "4", "5", "6", "7", "8", "10", "J", "Q", "K", "A"];
-  const palos = ["♦", " ♥", "♠", "♣"];
+  const palos = ["♦", "♥", "♠", "♣"];
+  const redColor = ["♦", "♥"];
 
   const randomValores = Math.floor(Math.random() * valores.length);
   const randomPalos = Math.floor(Math.random() * palos.length);
@@ -18,4 +19,8 @@ window.onload = function() {
   numero.innerText = valores[randomValores];
   const palo2 = document.getElementById("palo2");
   palo2.innerText = palos[randomPalos];
+  if (redColor.includes(palos[randomPalos])) {
+    palo1.classList.add("red");
+    palo2.classList.add("red");
+  }
 };
